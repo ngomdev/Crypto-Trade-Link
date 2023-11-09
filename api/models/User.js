7f22457312b250b88ab9f185eb565da1be17dc02
asseBadiane <badianeasse99@gmail.com>
2023-11-09 19:49:34 +0000

    Create the model User

---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "trader"],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
