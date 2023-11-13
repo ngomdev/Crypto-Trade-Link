d8798dc95f313975038fa72bdc18cd13d6bd93e0
asseBadiane <badianeasse99@gmail.com>
2023-11-13 20:37:30 +0000

    Create the function Sign-in
    Signed-off-by: asseBadiane <badianeasse99@gmail.com>

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -11,5 +13,23 @@ export const signup = async (req, res, next) => {
   } catch (err) {
     res.status(500).json(err.message);
     next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcrypt.compare(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong password!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
