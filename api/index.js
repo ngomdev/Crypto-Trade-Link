96187376fd0c4a5373b5b8e5fe3967397bbdcc94
asseBadiane <badianeasse99@gmail.com>
2023-11-16 21:25:15 +0000

    Add pre installation fix bug

--- api/index.js
+++ api/index.js
@@ -2,20 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import path from "path";
 
 import user from "./routes /user.route.js";
 import auth from "./routes /auth.route.js";
-
-const _dirname = path.resolve();
-app.use(express.static(path.join(_dirname, "Crypto-Trade-Link/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(_dirname, "Crypto-Trade-Link", "build", "index.html"));
-});
-
-
-const app = express();
-app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO, {
@@ -25,12 +15,23 @@ mongoose
   .then(() => console.log("Connection to MongoDB is Successfully✅ !"))
   .catch(() => console.log("Connection to MongoDB failed!"));
 
+const app = express();
+app.use(express.json());
+
 app.listen("3000", () => {
   console.log("Server running on port 3000");
 });
 
 app.use("/api/user", user);
 app.use("/api/auth", auth);
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "Crypto-Trade-Link/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "Crypto-Trade-Link", "build", "index.html"));
+});
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
