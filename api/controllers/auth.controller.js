b33838596b3e15b6ddc42e89e556e800f9dd5e44
asseBadiane <badianeasse99@gmail.com>
2023-11-16 20:52:08 +0000

    Add pre installation fix otp

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,6 @@
 import User from "../models/User.js";
-import OTP from "../models/OTP.js";
 import bcrypt from "bcrypt";
 import Jwt from "jsonwebtoken";
-import otpGenerator from "otp-generator";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -106,44 +104,3 @@ export const google = async (req, res, next) => {
   }
 }
 
-// Send OTP For Email Verification
-// export const sendotp = async (req, res) => {
-//   try {
-//     const { email } = req.body;
-
-//     // Check if user is already present
-//     // Find user with provided email
-//     const checkUserPresent = await User.findOne({ email });
-//     // If user found with provided email
-//     if (checkUserPresent)
-//       // Return 401 Unauthorized status code with error message
-//       return next(errorHandler(401, `User is Already Registered`));
-
-//     var otp = otpGenerator.generate(6, {
-//       upperCaseAlphabets: false,
-//       lowerCaseAlphabets: false,
-//       specialChars: false,
-//     });
-//     const result = await OTP.findOne({ otp: otp });
-//     console.log("Result is Generate OTP Func");
-//     console.log("OTP", otp);
-//     console.log("Result", result);
-//     while (result) {
-//       otp = otpGenerator.generate(6, {
-//         upperCaseAlphabets: false,
-//       });
-//     }
-//     const otpPayload = { email, otp };
-//     const otpBody = await OTP.create(otpPayload);
-//     console.log("OTP Body", otpBody);
-//     res.status(200).json({
-//       success: true,
-//       message: `OTP Sent Successfully`,
-//       otp,
-//     });
-//   } catch (error) {
-//     console.log(error.message);
-//     // return next(errorHandler(500, error.message));
-//     return res.status(500).json({ success: false, error: error.message });
-//   }
-// };
